d2927399d63b020fa4cdcd284d27aa095935e2df
Szabolcs Peter <szabi.peter@gmail.com>
2026-06-11 14:28:23 +0200

    Delete API inside Accounts

--- section2/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -7,4 +7,5 @@ public interface IAccountsService {
     void createAccount(CustomerDto customerDto);
     CustomerDto fetchAccount(String mobileNumber);
     boolean updateAccount(CustomerDto customerDto);
+    boolean deleteAccount(String mobileNumber);
 }
