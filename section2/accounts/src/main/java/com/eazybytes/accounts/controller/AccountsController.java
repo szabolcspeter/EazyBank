c3c07dd58b77a5e5fc2b086361f4a17836dd9f2b
Szabolcs Peter <szabi.peter@gmail.com>
2026-06-11 14:10:50 +0200

    Update API inside Accounts

--- section2/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -35,4 +35,18 @@ public class AccountsController {
                 .status(HttpStatus.OK)
                 .body(customerDto);
     }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto) {
+        boolean isUpdated = iAccountsService.updateAccount(customerDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+        }
+    }
 }
--- section2/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -6,4 +6,5 @@ public interface IAccountsService {
 
     void createAccount(CustomerDto customerDto);
     CustomerDto fetchAccount(String mobileNumber);
+    boolean updateAccount(CustomerDto customerDto);
 }
