364a60c1eab0f1f66161a2d29fa3470d273bad45
Szabolcs Peter <szabi.peter@gmail.com>
2026-06-11 13:48:15 +0200

    Read API in Accounts

--- section2/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -5,4 +5,5 @@ import com.eazybytes.accounts.dto.CustomerDto;
 public interface IAccountsService {
 
     void createAccount(CustomerDto customerDto);
+    CustomerDto fetchAccount(String mobileNumber);
 }
--- section2/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -1,10 +1,13 @@
 package com.eazybytes.accounts.service.impl;
 
 import com.eazybytes.accounts.constants.AccountsConstants;
+import com.eazybytes.accounts.dto.AccountsDto;
 import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.entity.Accounts;
 import com.eazybytes.accounts.entity.Customer;
 import com.eazybytes.accounts.exception.CustomerAlreadyExistsException;
+import com.eazybytes.accounts.exception.ResourceNotFoundException;
+import com.eazybytes.accounts.mapper.AccountsMapper;
 import com.eazybytes.accounts.mapper.CustomerMapper;
 import com.eazybytes.accounts.repository.AccountsRepository;
 import com.eazybytes.accounts.repository.CustomerRepository;
@@ -49,5 +52,21 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setCreatedAt(LocalDateTime.now());
         newAccount.setCreatedBy("Anonymous");
         return newAccount;
+    }
+
+    @Override
+    public CustomerDto fetchAccount(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+        );
+
+        CustomerDto customerDto = CustomerMapper.mapToCustomerDto(customer, new CustomerDto());
+        customerDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        return customerDto;
     }
 }
